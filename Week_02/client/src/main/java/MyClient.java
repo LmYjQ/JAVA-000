568f08cba1fbce36a2cad460ad1f2d1d8f44cb21
liuyiquan <liuyiquan@kanzhun.com>
2020-11-02 16:35:26 +0800

    tyop fix

--- Week_02/client/src/main/java/MyClient.java
+++ Week_02/client/src/main/java/MyClient.java
@@ -9,7 +9,7 @@ public class MyClient {
     public static void main(String[] args) throws IOException {
 
         OkHttpClient client = new OkHttpClient();
-        String url = "http://localhost:8801ta";
+        String url = "http://localhost:8801";
         Request request = new Request.Builder().url(url).build();
         try (Response response = client.newCall(request).execute()) {
             String resp = Objects.requireNonNull(response.body()).string();
